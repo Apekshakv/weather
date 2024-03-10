a25123eed920693a9840e73e1b8aba4447e9c1f8
Apeksha <153891838+Apekshakv@users.noreply.github.com>
2024-03-10 20:48:54 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -3,39 +3,36 @@ function getWeather() {
   const temperatureElement = document.getElementById('tem');
   const weatherInfoElement = document.getElementById('weather-info');
   const rat = document.getElementById('wind-info');
- 
+
   if (!selectedCity) {
     alert('Please select a city');
     return;
   }
-  const weathera = async (city) => {
-    const currentWeatherUrl = `https://goweather.herokuapp.com/weather/${city}`;
 
-    console.log(`Getting current weather for ${city}...`);
+  const weathera = async () => { 
+    const currentWeatherUrl = `https://goweather.herokuapp.com/weather/${selectedCity}`; 
+
+    console.log(`Getting current weather for ${selectedCity}...`);
     try {
       let response = await fetch(currentWeatherUrl);
       if (!response.ok) {
         throw { error: 'Network response was not ok' };
       }
       let data = await response.json();
-      displayWeather(data, city, temperatureElement, weatherInfoElement,rat);
-    } 
-    catch (error) {
-      console.error(`Error getting current weather for ${city}:`, error);
+      displayWeather(data, selectedCity, temperatureElement, weatherInfoElement, rat); 
+    } catch (error) {
+      console.error(`Error getting current weather for ${selectedCity}:`, error);
     }
   };
-  
-  weathera(selectedCity);
 
+  weathera();
 }
 
-function displayWeather(data, city, temperatureElement, weatherInfoElement,rat) {
-  temperatureElement.innerHTML =data.temperature//`<div>${data.temperature}</div>`;
-  weatherInfoElement.innerHTML = data.description
-  rat.innerHTML= `Wind: ${data.wind}`;
- 
+function displayWeather(data, city, temperatureElement, weatherInfoElement, rat) {
+  temperatureElement.innerHTML = data.temperature;
+  weatherInfoElement.innerHTML = data.description;
+  rat.innerHTML = `Wind: ${data.wind}`;
 }
 
-  
   
 
